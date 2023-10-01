9786e27e416edcbd1ce475379991ec3520a36d13
Diego-Pimenta <diegopimenta888@gmail.com>
2023-10-01 12:53:59 -0300

    User inserction with POST

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -1,14 +1,17 @@
 package com.nelioalves.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.UserDTO;
@@ -32,5 +35,13 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) { // aponta que a String id está localizada na url após o /users/
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj)); // o error precisa ser tratado para que o request get não devolva um 500, mas sim um 404
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // vai pegar o novo endereço do obj inserido
+		return ResponseEntity.created(uri).build(); // retorna o código 201, código de resposta http quando um novo recurso é criado 
 	}
 }
