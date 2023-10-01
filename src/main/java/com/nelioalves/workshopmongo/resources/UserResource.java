cd764f5213ee75105a179ec20f25cf282b9df204
Diego-Pimenta <diegopimenta888@gmail.com>
2023-10-01 12:31:57 -0300

    Getting an user by id

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); // converte cada obj da lista original para um DTO
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) { // aponta que a String id está localizada na url após o /users/
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj)); // o error precisa ser tratado para que o request get não devolva um 500, mas sim um 404
 	}
 }
